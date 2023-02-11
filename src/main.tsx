e07837648d17038e9a78c30665afc5956d7406b8
Nam Hoang Nguyen <nam.nh.2068@aptechlearning.edu.vn>
2023-02-12 03:15:42 +0700

    [Update] Merge brand review1102

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 
-import User from './pages/User/User'
-import Home from './pages/Home/Home'
+const User = React.lazy(() => import('./pages/User/User'))
+const Home = React.lazy(() => import('./pages/Home/Home'))
+const Layout = React.lazy(() => import('./components/Layout'))
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="signup" element={<User />}/>
-    </Routes>
-  </BrowserRouter>
+  <Suspense fallback={null}>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="test" element={<User />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  </Suspense>
 )
