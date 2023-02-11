93d37c173fb95ea139eebab4aec699aca46b07fc
Nam Hoang Nguyen <nam.nh.2068@aptechlearning.edu.vn>
2023-02-12 03:18:13 +0700

    [Update] Fix conflict branch

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,5 @@
-import SideBar from "../../components/SideBar"
-import NavBar from"../../components/NavBar"
-
 export default function Home () {
     return <div>
-        <NavBar></NavBar>
-        <SideBar></SideBar>
         <p>Hello world</p>
     </div>
 }
--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,9 +1,5 @@
-import SideBar from "../../components/SideBar"
-import NavBar from"../../components/NavBar"
-
 export default function User () {
     return <div>
-        <NavBar></NavBar>
-        <SideBar></SideBar>
+        User Page
     </div>
 }
