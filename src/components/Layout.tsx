d5d2ffc258645c476367554d53dd374bc8ecba2e
Nam Hoang Nguyen <nam.nh.2068@aptechlearning.edu.vn>
2023-02-12 03:02:36 +0700

    [Update] Route + Layouts

---
+++ src/components/Layout.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components"
+import Sidebar from "./SideBar"
+import Navbar from "./NavBar"
+
+const Main = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+`
+
+const RightSide = styled.div`
+    width: calc(100% - 16.46%);
+`
+
+const Player = styled.div`
+    position: fixed;
+    bottom: 0;
+    width: 100vw;
+    height: 10%;
+    min-height: 80px;
+    background: #797979;
+`
+
+interface iLayoutProps {
+    children: React.ReactElement
+}
+
+export default function Layout({ children }: iLayoutProps) {
+    return <Main>
+        <Sidebar />
+        <RightSide>
+            <Navbar />
+            {children}
+        </RightSide>
+        <Player />
+    </Main>
+}
