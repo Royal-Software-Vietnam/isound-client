49b25dbac66b1320165890e72bbc23518ab659ad
Nam Hoang Nguyen <nam.nh.2068@aptechlearning.edu.vn>
2023-03-02 08:30:43 +0700

    [init] Audio Player

--- src/components/Theme/Player.tsx
+++ src/components/Theme/Player.tsx
@@ -3,13 +3,13 @@ import { useRef } from "react"
 import ReactJkMusicPlayer, { ReactJkMusicPlayerAudioListProps } from 'react-jinke-music-player'
 import 'react-jinke-music-player/assets/index.css'
 import { useApp } from "../../context"
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
-const Main = styled.div`
-  position: fixed;
-  bottom: 0;
-  width: 100vw;
-  min-height: 80px;
-  background: #797979;
+const RhapPlayer = styled(AudioPlayer)`
+  &.rhap_container {
+    background: radial-gradient(rgba(0,0,0,0), #000000);
+  }
 `
 
 export default function Player () {
@@ -17,7 +17,12 @@ export default function Player () {
   const { mediaList }:any = useApp()
   const audioRef:any = useRef(null);
 
-  return <div>
-    <ReactJkMusicPlayer drag audioLists={mediaList} preload={true} defaultVolume={50} />
+  return <div className="fixed bottom-0 left-0 right-0">
+    {/* <ReactJkMusicPlayer drag audioLists={mediaList} preload={true} defaultVolume={50} /> */}
+    <RhapPlayer
+    autoPlay
+    src="https://www.youtube.com/watch?v=1dlTWaiBZDw"
+    preload="auto"
+    />
   </div>
 }
--- src/pages/Search/SearchAllData.tsx
+++ src/pages/Search/SearchAllData.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function SearchAllData({data}:{data:any}) {
   return (
