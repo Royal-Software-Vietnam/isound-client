539dea46c672de373cab7d8dca790600ef6a5b9f
Nam Hoang Nguyen <nam.nh.2068@aptechlearning.edu.vn>
2023-03-03 21:46:47 +0700

    [update] Complete UI player (mute animate missing)

--- src/components/Theme/Player.tsx
+++ src/components/Theme/Player.tsx
@@ -31,8 +31,11 @@ const RhapPlayer = styled(AudioPlayer)`
   }
 
   &.rhap_container svg:hover,
-  &.rhap_container svg:focus {
+  &.rhap_container svg:focus,
+  &.rhap_loop--on .rhap_repeat-button svg,
+  &.rhap_play-status--playing .rhap_play-pause-button svg {
     color: #E9003F;
+    opacity: 1;
   }
 
   .rhap_time {
@@ -41,9 +44,20 @@ const RhapPlayer = styled(AudioPlayer)`
     font-size: 0.75rem;
   }
 
+  .rhap_volume-filled,
+  .rhap_progress-filled {
+    background: #E9003F;
+  }
+
+  .rhap_volume-bar,
+  .rhap_progress-bar {
+    background: #2B2B2B;
+  }
+
   .rhap_volume-indicator,
   .rhap_progress-indicator {
     background: #E9003F;
+    opacity: 1;
   }
 `
 
@@ -56,8 +70,9 @@ export default function Player () {
     {/* <ReactJkMusicPlayer drag audioLists={mediaList} preload={true} defaultVolume={50} /> */}
     <RhapPlayer
       autoPlay
-      src={mediaList}
+      src="https://www.youtube.com/watch?v=NG3YyVNyKUk"
       preload="auto"
+      // onPlay={e => console.log("onPlay")}
       showSkipControls={true}
       showJumpControls={false}
       layout="horizontal-reverse"
@@ -67,6 +82,10 @@ export default function Player () {
         next: <StepForwardOutlined className="small"/>,
         pause: <PauseOutlined className="large"/>
       }}
+      loop={false}
+      muted={false}
+      showFilledVolume={true}
+      showFilledProgress={true}
     />
   </div>
 }
